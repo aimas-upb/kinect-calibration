c469cac36767997157c2d446c6aaf78d84c301bb
paula-gradu <paula.gradu@gmail.com>
2016-10-18 14:51:15 +0300

    Add files via upload

---
+++ Kinect Align Depth RGB/MainWindow.xaml.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Kinect;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Diagnostics;
+
+
+namespace Kinect_Align_Depth_RGB
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private KinectSensor _sensor;
+        private WriteableBitmap _bitmap;
+        private byte[] _bitmapBits;
+        private byte[] _depthBits;
+        private byte[] _myBits;
+        private WriteableBitmap _bitmapC;
+
+        private WriteableBitmap _myBitmap;
+        private byte[] _bitmapBitsC;
+        private ColorImagePoint[] _mappedDepthLocations;
+        private byte[] _colorPixels = new byte[0];
+        private byte[] _myPixels = new byte[0];
+        private byte[] _colorPixelsC = new byte[0];
+        private short[] _depthPixels = new short[0];
+        System.IO.StreamWriter file = new System.IO.StreamWriter("C:\\Users\\Titiana\\Desktop\\Calibration\\Camera1_mapping.csv");
+
+        void CreateThumbnail(string filename, BitmapSource image5)
+        {
+            if (filename != string.Empty)
+            {
+                using (FileStream stream5 = new FileStream(filename, FileMode.Create))
+                {
+                    PngBitmapEncoder encoder5 = new PngBitmapEncoder();
+                    encoder5.Frames.Add(BitmapFrame.Create(image5));
+                    encoder5.Save(stream5);
+                }
+            }
+        }
+
+        private void SetSensor(KinectSensor newSensor)
+        {
+            if (_sensor != null)
+            {
+                _sensor.Stop();
+            }
+
+            _sensor = newSensor;
+
+            if (_sensor != null)
+            {
+                Debug.Assert(_sensor.Status == KinectStatus.Connected, "This should only be called with Connected sensors.");
+                _sensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+                _sensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
+                _sensor.AllFramesReady += _sensor_AllFramesReady;
+                _sensor.Start();
+            }
+        }
+
+        void _sensor_AllFramesReady(object sender, AllFramesReadyEventArgs e)
+        {
+            bool gotColor = false;
+            bool gotDepth = false;
+
+            using (ColorImageFrame colorFrame = e.OpenColorImageFrame())
+            {
+                if (colorFrame != null)
+                {
+                    Debug.Assert(colorFrame.Width == 640 && colorFrame.Height == 480, "This app only uses 640x480.");
+
+                    if (_colorPixels.Length != colorFrame.PixelDataLength)
+                    {
+                        _colorPixels = new byte[colorFrame.PixelDataLength];
+                        _myPixels = new byte[colorFrame.PixelDataLength];
+                        _bitmap = new WriteableBitmap(640, 480, 96.0, 96.0, PixelFormats.Bgr32, null);
+                        _bitmapBits = new byte[640 * 480 * 4];
+                        this.Image.Source = _bitmap;
+                    }
+
+                    colorFrame.CopyPixelDataTo(_colorPixels);
+                    gotColor = true;
+                }
+            }
+
+            using (DepthImageFrame depthFrame = e.OpenDepthImageFrame())
+            {
+                if (depthFrame != null)
+                {
+                    Debug.Assert(depthFrame.Width == 640 && depthFrame.Height == 480, "This app only uses 640x480.");
+
+                    if (_depthPixels.Length != depthFrame.PixelDataLength)
+                    {
+                        _depthPixels = new short[depthFrame.PixelDataLength];
+                        _mappedDepthLocations = new ColorImagePoint[depthFrame.PixelDataLength];
+                    }
+
+                    depthFrame.CopyPixelDataTo(_depthPixels);
+                    gotDepth = true;
+                }
+            }
+
+            // Put the color image into _bitmapBits
+            //int c = _depthPixels.Length; 307200 * 4 = 1228800
+            for (int i = 0; i < _colorPixels.Length; i += 4) //1228800
+            {
+                _bitmapBits[i + 3] = 255;
+                _bitmapBits[i + 2] = _colorPixels[i + 2];
+                _bitmapBits[i + 1] = _colorPixels[i + 1];
+                _bitmapBits[i] = _colorPixels[i];
+            }
+
+            this._sensor.MapDepthFrameToColorFrame(DepthImageFormat.Resolution640x480Fps30, _depthPixels, ColorImageFormat.RgbResolution640x480Fps30, _mappedDepthLocations);
+
+            _bitmap.WritePixels(new Int32Rect(0, 0, _bitmap.PixelWidth, _bitmap.PixelHeight), _bitmapBits, _bitmap.PixelWidth * sizeof(int), 0);
+
+            CreateThumbnail("C:\\Users\\Titiana\\Desktop\\Calibration\\Camera1_rgb.bmp", _bitmap.Clone());
+
+            double[][] DepthMatrix = new double[640][];
+            for(int i = 0; i < 640; ++i)
+            {
+                DepthMatrix[i] = new double[480];
+            }
+            for (int i = 0; i < _depthPixels.Length; i++)
+            {
+                int depthVal = _depthPixels[i] >> DepthImageFrame.PlayerIndexBitmaskWidth;
+                ColorImagePoint point = _mappedDepthLocations[i];
+                DepthMatrix[point.X][point.Y] = depthVal;
+            }
+            
+            for (int i = 0; i < 640; i++) {
+                string line = "";
+                for (int j = 0; j < 480; j++)
+                {
+                    line += DepthMatrix[i][j].ToString();
+                    if(j < 479)
+                        line += ",";
+                }
+                line += "\n";
+                file.Write(line);
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            KinectSensor.KinectSensors.StatusChanged += (object sender, StatusChangedEventArgs e) =>
+            {
+                if (e.Sensor == _sensor)
+                {
+                    if (e.Status != KinectStatus.Connected)
+                    {
+                        SetSensor(null);
+                    }
+                }
+                else if ((_sensor == null) && (e.Status == KinectStatus.Connected))
+                {
+                    SetSensor(e.Sensor);
+                }
+            };
+
+            foreach (var sensor in KinectSensor.KinectSensors)
+            {
+                if (sensor.Status == KinectStatus.Connected)
+                {
+                    SetSensor(sensor);
+                }
+            }
+        }
+    }
+}
